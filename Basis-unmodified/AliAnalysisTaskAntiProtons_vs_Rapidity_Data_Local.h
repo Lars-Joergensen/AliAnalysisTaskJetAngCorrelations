f512bd3f12f99aca51618f821ff1bd1ae6a8a832
Lars-Joergensen <89340936+Lars-Joergensen@users.noreply.github.com>
2023-11-03 18:25:45 +0100

    Added working code
    Chiara's code, unmodified. This is the new basis now.

---
+++ Basis-unmodified/AliAnalysisTaskAntiProtons_vs_Rapidity_Data_Local.h
@@ -0,0 +1,121 @@
+#ifndef AliAnalysisTaskAntiProtons_vs_Rapidity_Data_Local_cxx
+#define AliAnalysisTaskAntiProtons_vs_Rapidity_Data_Local_cxx
+
+//======================== Antiprotons vs. Rapidity ========================//
+//                                                                          //
+//    Antiproton production vs. transverse momentum and rapidity            //
+//    for the calculation of the coalescence parameters B_{2} and B_{3}.    //
+//                                                                          //
+//==========================================================================//
+
+#include "AliAnalysisTaskSE.h"
+#include "AliMultSelection.h"
+#include "AliAnalysisUtils.h"
+#include "AliESDtrackCuts.h"
+#include "AliAnalysisTask.h"
+#include "AliPIDResponse.h"
+#include "AliESDVertex.h"
+#include "AliEventCuts.h"
+#include "AliESDEvent.h"
+#include "AliESDtrack.h"
+#include "THnSparse.h"
+#include "TList.h"
+#include "TH1F.h"
+#include "TH2F.h"
+#include "TH3F.h"
+
+//____________________________________________________________________________________________________________________________________________________
+class AliAnalysisTaskAntiProtons_vs_Rapidity_Data_Local : public AliAnalysisTaskSE {
+        
+public:
+    AliAnalysisTaskAntiProtons_vs_Rapidity_Data_Local();
+    AliAnalysisTaskAntiProtons_vs_Rapidity_Data_Local(const char *name);
+    virtual ~AliAnalysisTaskAntiProtons_vs_Rapidity_Data_Local();
+        
+    //Running Mode
+    void SetRunningMode (Bool_t isITSrecalib, Bool_t runData, Bool_t matchingEff) {
+        
+        fIsITSrecalib = isITSrecalib;
+        fRunData      = runData;
+        fMatchingEff  = matchingEff;
+    }
+    
+    //Set ITS Recalibration maps
+    void SetITSRecalibrationMaps (TH2F *hITSnsigma_Mean, TH2F *hITSnsigma_Width)  {
+        
+        hMean  = hITSnsigma_Mean;
+        hWidth = hITSnsigma_Width;
+    }
+    
+    //General Functions
+    virtual void UserCreateOutputObjects();
+    virtual void UserExec  (Option_t *option);
+    virtual void Terminate (Option_t *);
+    void  RunData();
+    void  MatchingEff();
+    
+    //User Functions
+    Bool_t   GetEvent ();
+    Bool_t   PassedTrackSelection        (AliESDtrack *track, Int_t isyst);
+    Bool_t   IsHighPurityProton          (AliESDtrack *track);
+    Bool_t   IsProtonCandidate           (AliESDtrack *track);
+    Double_t GetDCAtoPrimaryVertex       (AliESDtrack *track, Int_t index);
+    Double_t GetRapidity                 (AliESDtrack *track, Double_t mass);
+    Double_t GetRecalibratedITSnsigma    (Double_t nsigma, Double_t eta, Double_t p);
+    Bool_t   IsPionCandidate             (AliESDtrack *track);
+    Bool_t   PassedV0Selection           (AliESDv0 *V0);
+    Bool_t   PassedTrackSelectionV0daugh (AliESDtrack *track);
+    Bool_t   PassedAntiLambdaSelection   (AliESDv0 *V0);
+    Double_t GetDecayLengthV0            (AliESDv0 *V0);
+    Double_t MassLambda                  (TVector3 Ppion, TVector3 Pprot);
+    
+    //Standard Event Selection
+    AliEventCuts  fESDEventSelection;//
+
+private:
+    AliESDEvent     *fESDEvent;//!
+    AliPIDResponse  *fPIDResponse;//!
+    AliESDtrackCuts *fESDtrackCuts[50];//!
+    AliESDtrackCuts *fESDtrackCuts_V0daugh;//!
+    TList           *fOutputList;//!
+    TList           *fQAList;//!
+    Bool_t           fIsITSrecalib;//
+    Bool_t           fRunData;//
+    Bool_t           fMatchingEff;//
+    
+    TH2F *hMean;//
+    TH2F *hWidth;//
+
+    //Event Counter and Centrality Distribution
+    TH1F *hNumberOfEvents;//!
+    TH1F *hMultiplicity;//!
+    TH1I *hMultDistribution;//!
+    
+    //n-Dimensional Histograms
+    THnSparse *hTPCnsigma;//!
+    THnSparse *hTOFnsigma;//!
+    THnSparse *hDCAxy;//!
+    
+    //2D ITS Recalibration Map
+    TH3F *hITSnsigma;//!
+    
+    //n-Dimensional Histograms (y>0 vs. y<0)
+    THnSparse *hTPCnsigma_vs_rap;//!
+    THnSparse *hTOFnsigma_vs_rap;//!
+    
+    //Matching Efficiency
+    TH2F *hAntiprotonsTPC;//!
+    TH2F *hAntiprotonsTOF;//!
+
+    //QA
+    TH2F *hnSigmaProtons_vs_Pt;//!
+    
+    AliAnalysisTaskAntiProtons_vs_Rapidity_Data_Local(const AliAnalysisTaskAntiProtons_vs_Rapidity_Data_Local&);
+    AliAnalysisTaskAntiProtons_vs_Rapidity_Data_Local& operator=(const AliAnalysisTaskAntiProtons_vs_Rapidity_Data_Local&);
+        
+    ClassDef(AliAnalysisTaskAntiProtons_vs_Rapidity_Data_Local, 1);
+    
+};
+//____________________________________________________________________________________________________________________________________________________
+
+#endif
